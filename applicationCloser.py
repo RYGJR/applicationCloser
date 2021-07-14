044a91bb9430f0c340ff22a7fa76a8ff00dc8850
Rolando Gaddi Jr <77565717+RYGJR@users.noreply.github.com>
2021-07-14 20:22:26 +0800

    Add files via upload

---
+++ applicationCloser.py
@@ -0,0 +1,12 @@
+import os
+
+def openFile():
+    os.startfile()
+
+
+def closeFile():
+    try: 
+        os.system('TASKKILL /F /IM VALORANT-Win64-Shipping.exe') 
+    except Exception as e: print(e)
+
+closeFile()
